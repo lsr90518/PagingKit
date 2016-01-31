3ca089ab08cf1a8aea417d45b7447d45126235b3
FluffyLiu <lsr90518@users.noreply.github.com>
2016-01-31 16:34:10 +0900

    Update Paging.java

--- Paging.java
+++ Paging.java
@@ -12,8 +12,7 @@ import io.swagger.annotations.ApiModelProperty;
 @Builder
 public class Paging {
 
-    @ApiModelProperty(value = "offset", notes = "省略すると最初からです。")
     private Long offset;
-    @ApiModelProperty(value = "取得件数", notes = "省略すると10件、100件以上入れても内部で100に変換されます。")
+
     private Integer limit;
 }
