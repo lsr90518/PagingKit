b748491557a95eaf5448c6f23ef7d64df4d26e32
FluffyLiu <lsr90518@users.noreply.github.com>
2016-01-31 16:29:24 +0900

    Create Paging.java

---
+++ Paging.java
@@ -0,0 +1,19 @@
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Paging implements Dto {
+
+    @ApiModelProperty(value = "offset", notes = "省略すると最初からです。")
+    private Long offset;
+    @ApiModelProperty(value = "取得件数", notes = "省略すると10件、100件以上入れても内部で100に変換されます。")
+    private Integer limit;
+}
